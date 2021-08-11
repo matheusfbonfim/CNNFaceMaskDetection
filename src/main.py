94ee846be59cbd89fde62fc182bdd7742c85d758
Matheus Bonfim <matheusbonfim@alunos.utfpr.edu.br>
2021-08-11 10:21:03 -0300

    Change Database

--- src/main.py
+++ src/main.py
@@ -1,1 +1,33 @@
-print("Hello World")
+## IMPORTANDO BIBLIOTECAS
+
+import numpy as np
+# Sequential model 
+#from keras.models import Sequential
+# Bibliotecas - Camadas
+#from keras.layers import Dense, Activation,Conv2D,MaxPooling2D,Flatten, Dropout
+# Otimizador
+#from keras.optimizers import Adam
+# Funcão de custo
+#from keras.metrics import categorical_crossentropy
+#from keras.preprocessing.image import ImageDataGenerator
+# Matriz de confusão
+#from sklearn.metrics import confusion_matrix
+
+import itertools
+import os 
+import shutil
+import matplotlib.pyplot as plt
+
+path = '../database/annotations/'
+
+info_dataset = {
+    "image_path" : [],
+    "status_mask" : [],
+    "path_validation" : []
+}
+
+print((os.listdir(path))[0])
+
+
+
+
