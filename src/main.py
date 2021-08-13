b0fa3e395bff0066e9fba3d35789274e2d4b96c6
Matheus Bonfim <matheusbonfim@alunos.utfpr.edu.br>
2021-08-13 00:58:35 -0300

    Processing-data

--- src/main.py
+++ src/main.py
@@ -2,16 +2,16 @@
 
 import numpy as np
 # Sequential model 
-#from keras.models import Sequential
+from keras.models import Sequential
 # Bibliotecas - Camadas
-#from keras.layers import Dense, Activation,Conv2D,MaxPooling2D,Flatten, Dropout
+from keras.layers import Dense, Activation,Conv2D,MaxPooling2D,Flatten, Dropout
 # Otimizador
-#from keras.optimizers import Adam
+# from keras.optimizers import Adam
 # Funcão de custo
-#from keras.metrics import categorical_crossentropy
-#from keras.preprocessing.image import ImageDataGenerator
+from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
 # Matriz de confusão
-#from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix
 
 import itertools
 import os 
@@ -22,21 +22,32 @@ import matplotlib.pyplot as plt
 
 
 #########################
-## PREPARANDO OS DADOS
+## PRÉ-PROCESSAMENTO DE DADOS DE IMAGEM
 
 # Dataset: https://www.kaggle.com/omkargurav/face-mask-dataset
 #   Organizaremos nossos dados em conjuntos de treinamento, validação e teste -> Manualmente
 #   Movendo subconjuntos de dados para subdiretórios para cada conjunto de dados separado.
 
-# ----------
-# Diretório
-#   Localização dos diferentes conjuntos de dados
+# Caminhos para os dados de treinamento, validação e teste
+path_train = '../database/Face Mask Dataset/Train'        # Caminho - Train
+path_valid = '../database/Face Mask Dataset/Test'   # Caminho - Validação
+path_test = '../database/Face Mask Dataset/Validation'          # Caminho - Teste
 
 
+# -------------------------
+### Gerando o conjunto de dados de treinamento, validação/teste
 
-path = '../database/annotations/'
+train_gen = ImageDataGenerator(rescale = 1.0/255.0)
+valid_gen = ImageDataGenerator(rescale = 1.0/255.0)
+test_gen =  ImageDataGenerator(rescale = 1.0/255.0)
+
+train_batches = train_gen.flow_from_directory(directory = path_train, batch_size = 64 , color_mode= 'rgb' ,target_size=(200, 200), class_mode = 'binary')
+valid_batches = valid_gen.flow_from_directory(directory = path_valid, batch_size = 64 , color_mode= 'rgb' ,target_size=(200, 200), class_mode = 'binary')
+test_batches = test_gen.flow_from_directory(directory = path_test, batch_size = 64,  color_mode= 'rgb', target_size=(200, 200), class_mode = 'binary')
 
 
+# Vendo os rotulos e os dados gerados
+train_batches.class_indices # Rede neural precisa de indices categoricos em formato inteiro
 
 
 
