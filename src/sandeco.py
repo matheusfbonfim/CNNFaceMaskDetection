fdca25304b3b2bef3004144c22f355a49a68bc56
Matheus de Farias Bonfim <matheusbonfim@alunos.utfpr.edu.br>
2021-08-21 11:44:01 -0300

    Update .py

--- src/sandeco.py
+++ src/sandeco.py
@@ -6,6 +6,7 @@ from mtcnn.mtcnn import MTCNN
 
 cap = cv2.VideoCapture(0)
 model = load_model('../model/new-face-mask-detection.h5')
+# model = load_model('../model/detector.h5')
 size = (200, 200)
 detector = MTCNN()
 
@@ -23,7 +24,7 @@ while True:
 
         if np.sum([roi]) != 0:
 
-            roi = cv2.resize(frame, size)
+            roi = cv2.resize(roi, size)
             roi = (roi.astype('float')/255.0)
 
             roi = np.reshape(roi, [1, 200, 200, 3])
@@ -32,21 +33,21 @@ while True:
 
             pred = pred[0]
 
-    print(pred)
+            print(pred)
 
-    if pred[0] >= pred[1]:
-        label = 'NO MASK'
-        color = (0,0,255)
-    else:
-        label = 'MASK'
-        color = (0, 255, 0)
+            if pred[0] >= pred[1]:
+                label = 'NO MASK'
+                color = (0,0,255)
+            else:
+                label = 'MASK'
+                color = (0, 255, 0)
 
-        cv2.rectangle(frame, (120, 120), (120, 120), color, 2)
-        cv2.putText(frame, label, (120, 120 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)
 
 
 
-    cv2.imshow("MATHEUS MASK DETECTOR", frame)
+    cv2.imshow("MASK DETECTOR", frame)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
